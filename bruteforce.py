84c7168b2d17b55c5442bdc7bd5f6fb3b5750467
philippe_pras <ph.pras@gmail.com>
2022-11-02 12:00:39 +0100

    Update

--- bruteforce.py
+++ bruteforce.py
@@ -1,66 +1,75 @@
+import math
 import csv
 import pprint
 import sys #K systemes
-from collections import defaultdict
+from collections import namedtuple
 import time
 
-def read_csv_file(csv_filename): #Recup des valeurs du shares.csv
+Share = namedtuple("Share", ["name", "price", "rate", "performance"]) # Creation d'un namedtuple(Attributs)
+
+def read_csv_file(csv_filename): #Recup des valeurs du fichier.csv indiqué en entrée et on calcul directement les performances au chargement(Car on ne peut acheter qu'une seule action de chaque)
     shares = []
     with open(csv_filename) as csvfile:
         reader = csv.reader(csvfile, delimiter=";")
+        next(reader) # Skip header du csv
         for row in reader:
-            shares.append([*map(float, row)])
+            name = row[0]
+            price, rate = map(float, row[1:])
+            share = Share(name, price, rate, round(price * (rate / 100), 2)) # 2chiffres apres la virgule
+            shares.append(share)
     return shares
 
 def get_max_performance(shares, max_amount):
     start = time.time()
-    # Ici on trie les actions de la plus performante à la moins perforamte selon leurs rendement 
-    performances = sorted( 
-        [(round(price * (rate / 100), 2), price, rate) for price, rate in shares],
-        key=lambda e: e[0],
-        reverse=True
-    )
+    # Ici on trie les actions de la plus performante à la moins performante selon leurs rendement
+    performances = sorted(shares, key=lambda share: share.performance,  reverse=True)
+    
     # Ici on stock la performance maximum et la liste des actions qui correspondent
     max_performance = 0
+    max_spent_amount = 0
     max_result = None
     i = 0
     while i < len(performances):
-        amount = max_amount
-        j = 0 
+        if performances[i].price < 0: #Skip prix négatifs
+            i += 1
+            continue
+        amount = max_amount - performances[i].price
+        j = 0
         result = [performances[i]]
-        performance = performances[i][0]
+        performance = performances[i].performance
         while j < len(performances):
             if i == j:
                 j += 1
                 continue
-            # On verifie qu'il reste de l'argent à placer
-            if amount >= performances[j][1] and performances[j][1] > 0: #Verification du dataset (valeurs Négatives)
+            # Ici on verifie qu'il reste de l'argent à placer
+            if amount >= performances[j].price and performances[j].price > 0: # Verification du dataset (valeurs Négatives)
                 result.append(performances[j])
-                amount -= performances[j][1]
-                performance += performances[j][0]
+                amount -= performances[j].price
+                performance += performances[j].performance
             j += 1
-        # Onsauvegarde la performance maximale si besoin
+        # On sauvegarde la performance maximale si besoin
         if performance > max_performance:
-            max_result = result 
+            max_result = list(result)
             max_performance = performance
+            max_spent_amount = amount
         i += 1
-    # on imprim les résultats
     end = time.time()
-    print(f"Avec {max_amount} euros d'investissement : " )
+    # on imprime les résultats
+    print(f"\nAvec {max_amount} euros d'investissement, vous pouvez maximiser votre randement en achetant : \n" )
     for share in max_result:
-        print(f"1 action à {share[1]} euros à {share[2]} % sur 2 ans (Performance = {share[0]} euros)")
-    print(f"Performance totale : {round(max_performance, 2)} euros")
-    print(f"Temps d'éxecution du programme : {end-start} secondes")
+        print(f"> 1 action -{share.name}- à {share.price} euros à {share.rate} % sur 2 ans (Performance = {share.performance} euros)")
+    print(f"\n> Performance totale : {round(max_performance, 2)} euros\n")
+    print(f"Total dépensé {max_amount - max_spent_amount} euros \n")
+    print(f"> Temps d'éxecution du programme : {end-start} secondes \n")
 
-
-
-
+# Lancement du programme avec nom du fichier.py choixdudataset.csv et montant choisi
 def main(args):
     if len(args) < 3:
         print(f"usage : {args[0]} csv_file max_amount", file=sys.stderr)
         raise SystemExit(-1)   
     csv_filename, max_amount, *_ = args[1:]
-    print(csv_filename, max_amount)
+    print()
+    print(f"Calcul du rendement sur le dataset : {csv_filename} avec un montant investi de : {max_amount} euros")
     shares = read_csv_file(csv_filename)
     get_max_performance(shares, int(max_amount))
 
